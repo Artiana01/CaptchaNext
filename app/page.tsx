7c9c384319044bfe861d2e0583ed80e3f63aa52c
Rojo Tiana <127603221+Artiana01@users.noreply.github.com>
2024-12-19 08:00:27 +0300

    captcha

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// app/captcha/page.jsx
+import CaptchaComponent from "@/app/component/captchaComponent";
+
+export default function CaptchaPage() {
+  return (
+    <main>
+      <h1>Intégration AWS WAF CAPTCHA</h1>
+      <CaptchaComponent />
+    </main>
+  );
+}
